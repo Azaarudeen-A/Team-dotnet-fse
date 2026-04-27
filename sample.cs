35eeb51ad4d61803f5484116f759b2d0fda2f8a9
S <2499214@cognizant.com>
2026-04-27 02:56:26 -0700

    Your message

--- sample.cs
+++ sample.cs
@@ -2,9 +2,17 @@ using System;
 
 class Program
 {
-    static void Main()
-    {
-        Console.WriteLine("Hello, World!");
-        Console.ReadLine();
-    }
+    
+static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is my first simple C# project.");
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Welcome, {name}!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
 }
